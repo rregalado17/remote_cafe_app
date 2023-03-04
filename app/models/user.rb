d814a1e81ba9af7dd8b14eff1105fdf8f1f89296
Robinson Regalado <rregalado17@gmail.com>
2023-03-03 19:59:53 -0600

    draft homepage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  validates :email, :username, presence: true, uniqueness: true 
   has_secure_password
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_15_044425) do
+ActiveRecord::Schema.define(version: 2023_02_15_045053) do
 
   create_table "cafes", force: :cascade do |t|
     t.string "title"
@@ -23,10 +23,12 @@ ActiveRecord::Schema.define(version: 2023_02_15_044425) do
   end
 
   create_table "reviews", force: :cascade do |t|
+    t.integer "user_id", null: false
     t.string "title"
     t.text "review"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +40,5 @@ ActiveRecord::Schema.define(version: 2023_02_15_044425) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "reviews", "users"
 end

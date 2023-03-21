e33fb595036ba62d13115124be0626d0a9290773
Robinson Regalado <rregalado17@gmail.com>
2023-03-20 19:43:41 -0600

    update turbolinks to show maps when moving from page to page dynamically

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,5 @@
 class PagesController < ApplicationController
-
-
-def home
-    redirect_to cafes_path if logged_in?
-end
-
-
-
+    def home
+        # redirect_to cafes_path if logged_in?
+    end
 end

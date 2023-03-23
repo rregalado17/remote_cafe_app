b601b2fe6d601e901251195af7af4dfe97da54cf
Robinson Regalado <rregalado17@gmail.com>
2023-03-22 22:49:17 -0600

    allow users to add a cover photo when making a cafe

--- app/models/cafe.rb
+++ app/models/cafe.rb
@@ -1,6 +1,7 @@
 class Cafe < ApplicationRecord
     belongs_to :user 
     has_many :reviews
+    has_one_attached :cover
     validates :title, :description, :longitude, :latitude, presence: true
     scope :recent, -> { order(created_at: :desc).limit(10) }
 end
